0ad79d8d0cd6af22ce6c860865652719d36c626e
Luca243153 <luca.bresolin@escola.pr.gov.br>
2024-12-10 15:44:14 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(600, 600);
+    background("pink")
+  }
+    
+  function draw() {
+    
+    
+    fill("white");
+    
+    // console.log(mouseIsPressed);
+    
+    if(mouseIsPressed){
+    rect(mouseX,mouseY, 30, 45);}
+    
+    stroke("pink");
+    
+    
+  }
+  
